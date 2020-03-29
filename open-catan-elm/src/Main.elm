028d38adc4870b0241729b3ef4d9cb73bcdf273c
Jakubcová Beáta <jakubcova@jakubcovamac2.local>
2020-03-29 14:17:06 +0200

    Send register API call

--- open-catan-elm/src/Main.elm
+++ open-catan-elm/src/Main.elm
@@ -1,20 +1,31 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes as Attributes exposing (src)
+import Html.Events as Events
+import Http
+import Json.Encode as Encode
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { data : Data }
+
+
+type alias Data =
+    { username : String
+    , email : String
+    , password : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { data = Data "name" "email" "password" }, Cmd.none )
 
 
 
@@ -22,12 +33,44 @@ init =
 
 
 type Msg
-    = NoOp
+    = Register
+    | RegisterResponse (Result Http.Error ())
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Register ->
+            ( model, registerCmd model.data )
+
+        RegisterResponse _ ->
+            ( model, Cmd.none )
+
+
+registerCmd : Data -> Cmd Msg
+registerCmd data =
+    let
+        url =
+            "http://localhost:3000" ++ "/register"
+    in
+    Http.request
+        { method = "POST"
+        , headers = [ Http.header "Content-Type" "application/json" ]
+        , url = url
+        , body = Http.jsonBody <| dataEncoder data
+        , expect = Http.expectWhatever RegisterResponse
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+dataEncoder : Data -> Encode.Value
+dataEncoder { username, email, password } =
+    Encode.object
+        [ ( "username", Encode.string username )
+        , ( "email", Encode.string email )
+        , ( "password", Encode.string password )
+        ]
 
 
 
@@ -38,7 +81,9 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 []
+            [ Html.button [ Events.onClick Register ] []
+            ]
         ]
 
 
